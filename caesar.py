d312f511d3f7462f5dbdb2dbb5de4dd63b96888b
mherna42 <49410553+mherna42@users.noreply.github.com>
2019-05-20 17:18:25 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,15 @@
 from helpers import alphabet_position, rotate_character
 
 def encrypt(text, rot):
-    #Add your code here
-    return
+    mixed_string = ""
+    for i in text:
+        mixed_string += (rotate_character(i, rot))
+    return mixed string    
+    
+def main():
+    text = input("Type a message: ")
+    rot = int(input("How much do you want to rotate by? ")
+    print(encrypt(text, rot))
+              
+if __name__=="__main__":
+    main()
